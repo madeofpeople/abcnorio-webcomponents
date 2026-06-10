0fd3f16eaa8461bef6a048e152c7db47169118de
Thiago de Mello Bueno <thiago@madeofpeople.org>
2026-06-10 14:40:41 +0000

    adds type declarations

---
+++ src/types/index.d.ts
@@ -0,0 +1,59 @@
+declare module 'abcnorio-webcomponents/event-listing' {
+  const Component: any;
+  export default Component;
+}
+
+declare module 'abcnorio-webcomponents/wp-blocks/paragraph' {
+  const Component: any;
+  export default Component;
+}
+
+declare module 'abcnorio-webcomponents/wp-blocks/heading' {
+  const Component: any;
+  export default Component;
+}
+
+declare module 'abcnorio-webcomponents/wp-blocks/image' {
+  const Component: any;
+  export default Component;
+}
+
+declare module 'abcnorio-webcomponents/wp-blocks/quote' {
+  const Component: any;
+  export default Component;
+}
+
+declare module 'abcnorio-webcomponents/wp-blocks/list' {
+  const Component: any;
+  export default Component;
+}
+
+declare module 'abcnorio-webcomponents/wp-blocks/list-item' {
+  const Component: any;
+  export default Component;
+}
+
+declare module 'abcnorio-webcomponents/wp-blocks/gallery' {
+  const Component: any;
+  export default Component;
+}
+
+declare module 'abcnorio-webcomponents/wp-blocks/youtube' {
+  const Component: any;
+  export default Component;
+}
+
+declare module 'abcnorio-webcomponents/wp-blocks/matterport' {
+  const Component: any;
+  export default Component;
+}
+
+declare module 'abcnorio-webcomponents/wp-blocks/buttons' {
+  const Component: any;
+  export default Component;
+}
+
+declare module 'abcnorio-webcomponents/wp-blocks/group' {
+  const Component: any;
+  export default Component;
+}
